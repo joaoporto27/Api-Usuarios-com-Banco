8803f5bb7e6af6a2abe5a5e1b0024777a327cdbc
joaoporto27 <joao.v.sales15@aluno.senai.br>
2025-03-11 10:57:15 -0300

    first commit

---
+++ src/database/schema.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE cadastro;
+
+\c cadastro
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL
+);
+
+INSERT INTO users (name, email) VALUES 
+    ('Thiago Ferreira', 'thiago.ferreira@email.com'),
+    ('Marcelo Carboni', 'marcelo@email.com'),
+    ('Eduardo Correia', 'eduardo@email.com'),
+    ('Felipe Santos', 'felipe@email');
