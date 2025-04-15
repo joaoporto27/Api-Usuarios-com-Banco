4725931939258f171d6363e14e23fd70e46b5c2f
joaoporto27 <joao.v.sales15@aluno.senai.br>
2025-04-15 16:00:37 -0300

    Feat: swagger

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const userRoutes = require("./src/routes/userRoutes");
+const setupSwagger = require("./src/config/swagger"); // Swagger aqui
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+setupSwagger(app); // Ativa o Swagger
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/api", userRoutes);
 
--- src/database/schema.sql
+++ src/database/schema.sql
@@ -5,7 +5,8 @@ CREATE DATABASE cadastro;
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL
+    email VARCHAR(100) UNIQUE NOT NULL;
+    photo TEXT
 );
 
 INSERT INTO users (name, email) VALUES 
